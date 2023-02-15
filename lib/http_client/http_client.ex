05300df01ca8336da456c328baeabad90cadaa2b
Honggang Wang <honggangwang@tubi.tv>
2023-02-15 11:43:37 +0800

    replaced compile_env to get_env

--- lib/http_client/http_client.ex
+++ lib/http_client/http_client.ex
@@ -1,11 +1,8 @@
 defmodule HttpMeta.HttpClient do
-  @client_impl Application.compile_env(:http_meta, :http_client, HttpMeta.HttpClientImpl)
-
   @callback get_ip() :: {:ok, String.t()}
 
-  def get_client() do
-    @client_impl
+  def get_ip() do
+    impl = Application.get_env(:http_meta, :http_client, HttpMeta.HttpClientImpl)
+    impl.get_ip()
   end
-
-  defdelegate get_ip, to: @client_impl
 end
